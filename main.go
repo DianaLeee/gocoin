a6743ffc31d694c25ae147d95fa84f511f12dbc9
Diana Lee <yeonylee94@gmail.com>
2022-11-06 15:01:06 +0900

    #6.1 Marshal & FieldText

--- main.go
+++ main.go
@@ -5,16 +5,16 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/DianaLeee/gocoin/utils"
 )
 
 const port string = ":4000";
 
+// omitempty -> hide the field
 type URLDescription struct {
-	URL string
-	Method string
-	Description string
+	URL string `json:"url"` // JSON일 때 (소문자)url로 변경된다는 뜻
+	Method string `json:"method"`
+	Description string `json:"description"`
+	Payload string `json:"payload,omitempty"` // Field가 비어있으면 field를 숨겨줌.
 }
 
 func documentation(rw http.ResponseWriter, r *http.Request) {
@@ -24,11 +24,15 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Method: "GET",
 			Description: "See Documentation",
 		},
+		{
+			URL: "/blocks",
+			Method: "POST",
+			Description: "Add a Block",
+			Payload: "data:string",
+		},
 	}
-	// Parsing JSON using Marshal
-	b, err := json.Marshal(data);
-	utils.HandleErr(err)
-	fmt.Printf("%s", b);
+	rw.Header().Add("Content-Type", "application/json") // Inform to header that return content type is json
+	json.NewEncoder(rw).Encode(data); // Parsing data as JSON type
 }
 
 func main() {
