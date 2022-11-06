31e0db377bc1761668ecac5bed1a01603581d714
Diana Lee <yeonylee94@gmail.com>
2022-11-06 14:46:09 +0900

    #6.0

--- main.go
+++ main.go
@@ -1,8 +1,39 @@
 package main
 
-import "github.com/DianaLeee/gocoin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/DianaLeee/gocoin/utils"
+)
+
+const port string = ":4000";
+
+type URLDescription struct {
+	URL string
+	Method string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription {
+		{
+			URL: "/",
+			Method: "GET",
+			Description: "See Documentation",
+		},
+	}
+	// Parsing JSON using Marshal
+	b, err := json.Marshal(data);
+	utils.HandleErr(err)
+	fmt.Printf("%s", b);
+}
 
 func main() {
-	explorer.Start();
+	http.HandleFunc("/", documentation);
+	fmt.Printf("Listening on http://localhost%s\n", port);
+	log.Fatal(http.ListenAndServe(port, nil));
+
 }
