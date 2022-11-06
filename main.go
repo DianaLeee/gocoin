1ce60db9dc28bb2875edf113c4f7d39c4f18939b
Diana Lee <yeonylee94@gmail.com>
2022-11-06 15:17:08 +0900

    #6.2 implement and MarshalText

--- main.go
+++ main.go
@@ -9,22 +9,39 @@ import (
 
 const port string = ":4000";
 
+type URL string;
+
+/*
+	interface
+	- 붕어빵 틀 같은거지...
+	- interface 사용하면 어떻게 출력될지 정할 수도 있음.
+	-  Implement하고 싶으면 그냥 사용하면 되고, JAVA처럼 implement 선언해주지 않아도 된다.
+*/
+// Implement the MarshalText method
+// intercepting original MarshalText function
+func (u URL) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u);
+	return []byte(url), nil;
+}
+
 type URLDescription struct {
-	URL string `json:"url"` // JSON일 때 (소문자)url로 변경된다는 뜻
+	URL URL `json:"url"` // JSON일 때 (소문자)url로 변경된다는 뜻
 	Method string `json:"method"`
 	Description string `json:"description"`
 	Payload string `json:"payload,omitempty"` // Field가 비어있으면 field를 숨겨줌.
 }
 
+
+
 func documentation(rw http.ResponseWriter, r *http.Request) {
 	data := []URLDescription {
 		{
-			URL: "/",
+			URL: URL("/"),
 			Method: "GET",
 			Description: "See Documentation",
 		},
 		{
-			URL: "/blocks",
+			URL: URL("/blocks"),
 			Method: "POST",
 			Description: "Add a Block",
 			Payload: "data:string",
@@ -35,6 +52,11 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	fmt.Println(URLDescription{
+		URL: "/",
+		Method: "GET",
+		Description: "See Documentation",
+	})
 	http.HandleFunc("/", documentation);
 	fmt.Printf("Listening on http://localhost%s\n", port);
 	log.Fatal(http.ListenAndServe(port, nil));
